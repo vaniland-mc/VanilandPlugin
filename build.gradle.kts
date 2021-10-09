4a5790398900988c38533c1a87ecbc5a8589dbe2
rona-tombo <ronatombo@gmail.com>
2021-10-10 04:04:11 +0900

    🎉 Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
+
+group = "land.vani.plugin"
+version = "0.1.0"
+
+val targetJavaVersion = 16
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "papermc-repo"
+        setUrl("https://papermc.io/repo/repository/maven-public/")
+    }
+    maven {
+        name = "sonatype"
+        setUrl("https://oss.sonatype.org/content/groups/public/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
