fd240c50dd4a78e8f7569170a17b1112c273e800
rona-tombo <ronatombo@gmail.com>
2021-10-10 15:45:15 +0900

    ➕ Add ktor-client-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,10 @@ dependencies {
 
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+    implementation("io.ktor:ktor-client-core:1.6.4")
+    implementation("io.ktor:ktor-client-serialization:1.6.4")
+    implementation("io.ktor:ktor-client-cio:1.6.4")
 }
 
 tasks {
