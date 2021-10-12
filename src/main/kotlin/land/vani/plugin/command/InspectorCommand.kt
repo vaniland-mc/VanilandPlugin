6da0d7e09f2d6daf7fd4e39769d04064b74309b5
rona-tombo <ronatombo@gmail.com>
2021-10-13 06:20:41 +0900

    ✨ Add inspector teleport

--- src/main/kotlin/land/vani/plugin/command/InspectorCommand.kt
+++ src/main/kotlin/land/vani/plugin/command/InspectorCommand.kt
@@ -2,9 +2,14 @@ package land.vani.plugin.command
 
 import com.github.syari.spigot.api.command.command
 import com.github.syari.spigot.api.command.tab.CommandTabArgument.Companion.argument
+import de.myzelyam.api.vanish.VanishAPI
 import land.vani.plugin.VanilandPlugin
 import land.vani.plugin.permission.INSPECTOR_MODE
+import net.kyori.adventure.extra.kotlin.text
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
+import org.bukkit.GameMode
+import org.bukkit.entity.Player
 
 fun VanilandPlugin.inspectorCommand() {
     command("inspector") {
@@ -21,7 +26,67 @@ fun VanilandPlugin.inspectorCommand() {
         }
 
         execute {
+            if (sender !is Player) {
+                sender.sendMessage(
+                    text {
+                        content("このコマンドはゲーム内からのみ実行できます")
+                        color(NamedTextColor.RED)
+                    }
+                )
+                return@execute
+            }
+            val player = sender as Player
 
+            if (args.size != 2) {
+                sender.sendMessage(
+                    text {
+                        content("引数が不正です")
+                        color(NamedTextColor.RED)
+                    }
+                )
+            }
+            when (args.lowerOrNull(0)) {
+                "tp" -> {
+                    val targetName = args.lowerOrNull(1)
+                    if (targetName.isNullOrEmpty()) {
+                        sender.sendMessage(
+                            text {
+                                content("対象のプレイヤーを指定してください")
+                                color(NamedTextColor.RED)
+                            }
+                        )
+                        return@execute
+                    }
+                    val target = Bukkit.getPlayer(targetName)
+                    if (target == null) {
+                        sender.sendMessage(
+                            text {
+                                content("プレイヤーが見つかりませんでした")
+                                color(NamedTextColor.RED)
+                            }
+                        )
+                        return@execute
+                    }
+
+                    player.gameMode = GameMode.SPECTATOR
+                    VanishAPI.hidePlayer(player)
+                    player.teleport(target.location)
+                    sender.sendMessage(
+                        text {
+                            content("透明化を有効にして${target.name}にテレポートしました")
+                            color(NamedTextColor.GREEN)
+                        }
+                    )
+                }
+                else -> {
+                    sender.sendMessage(
+                        text {
+                            content("不正なサブコマンドです")
+                            color(NamedTextColor.RED)
+                        }
+                    )
+                }
+            }
         }
     }
 }
