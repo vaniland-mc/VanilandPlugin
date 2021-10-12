d1995047c75519995e056ed802a4d0ff48bcaeb5
rona-tombo <ronatombo@gmail.com>
2021-10-12 21:32:40 +0900

    🔥 Remove unnecessary suppression

--- src/main/kotlin/land/vani/plugin/bukkit/VanilandPlugin.kt
+++ src/main/kotlin/land/vani/plugin/bukkit/VanilandPlugin.kt
@@ -3,7 +3,6 @@ package land.vani.plugin.bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class VanilandPlugin: JavaPlugin() {
-    @Suppress("EXPERIMENTAL_API_USAGE_FUTURE_ERROR")
     override fun onEnable() {
 
     }
