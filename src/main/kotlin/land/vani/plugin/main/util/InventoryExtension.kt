622e5562d4b6c7a46528abde9bb49c131f1cffcc
rona-tombo <ronatombo@gmail.com>
2021-11-19 07:24:03 +0900

    🔧 Add newbie message and items

--- src/main/kotlin/land/vani/plugin/main/util/InventoryExtension.kt
+++ src/main/kotlin/land/vani/plugin/main/util/InventoryExtension.kt
@@ -3,9 +3,11 @@ package land.vani.plugin.main.util
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
-fun Player.giveItemOrDrop(vararg itemStack: ItemStack) {
-    val flowedItems = inventory.addItem(*itemStack).values
+fun Player.giveItemOrDrop(vararg itemStacks: ItemStack) {
+    val flowedItems = inventory.addItem(*itemStacks).values
     flowedItems.forEach {
         world.dropItemNaturally(location, it)
     }
 }
+
+fun <T : Collection<ItemStack>> Player.giveItemOrDrop(itemStacks: T) = giveItemOrDrop(*itemStacks.toTypedArray())
