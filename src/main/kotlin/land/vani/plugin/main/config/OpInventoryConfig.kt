9e96ff5ab5af1c0012885453545e82f208f2678f
rona-tombo <ronatombo@gmail.com>
2021-11-22 07:44:20 +0900

    🐛 Fix OP inventory

--- src/main/kotlin/land/vani/plugin/main/config/OpInventoryConfig.kt
+++ src/main/kotlin/land/vani/plugin/main/config/OpInventoryConfig.kt
@@ -2,25 +2,62 @@ package land.vani.plugin.main.config
 
 import com.github.syari.spigot.api.config.CustomConfig
 import com.github.syari.spigot.api.config.type.ConfigDataType
+import com.github.syari.spigot.api.item.itemStack
+import org.bukkit.Material
 import org.bukkit.entity.Player
-import org.bukkit.inventory.Inventory
-import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.PlayerInventory
+import java.util.UUID
 
 class OpInventoryConfig(
     private val inner: CustomConfig,
 ) {
-    fun getOpInventory(player: Player): Array<ItemStack?>? =
-        inner.get("op.${player.uniqueId}", ConfigDataType.SerializableInventory, false)
-
-    fun setOpInventory(player: Player, inventory: Inventory) {
-        inner.set("op.${player.uniqueId}", ConfigDataType.SerializableInventory, inventory.contents)
+    fun applyOpInventoryToPlayer(player: Player) {
+        applyInventory(player, "op")
     }
 
-    fun getPlayerInventory(player: Player): Array<ItemStack?>? =
-        inner.get("player.${player.uniqueId}", ConfigDataType.SerializableInventory, false)
+    fun setOpInventory(player: Player, inventory: PlayerInventory) {
+        setInventory(player.uniqueId, inventory, "op")
+    }
 
-    fun setPlayerInventory(player: Player, inventory: Inventory) {
-        inner.set("player.${player.uniqueId}", ConfigDataType.SerializableInventory, inventory.contents)
+    fun applyPlayerInventoryToPlayer(player: Player) {
+        applyInventory(player, "player")
+    }
+
+    fun setPlayerInventory(player: Player, inventory: PlayerInventory) {
+        setInventory(player.uniqueId, inventory, "player")
+    }
+
+    private fun setInventory(uuid: UUID, inventory: PlayerInventory, prefix: String) {
+        inventory.armorContents.forEachIndexed { index, itemStack ->
+            inner.set("$prefix.$uuid.armor.$index", ConfigDataType.SerializableItemStack, itemStack)
+        }
+        inventory.storageContents.forEachIndexed { index, itemStack ->
+            inner.set("$prefix.$uuid.storage.$index", ConfigDataType.SerializableItemStack, itemStack)
+        }
+        inventory.extraContents.forEachIndexed { index, itemStack ->
+            inner.set("$prefix.$uuid.extra.$index", ConfigDataType.SerializableItemStack, itemStack)
+        }
+        inner.save()
+    }
+
+    private fun applyInventory(player: Player, prefix: String) {
+        val armorContents = Array(player.inventory.armorContents.size) {
+            inner.get("$prefix.${player.uniqueId}.armor.$it", ConfigDataType.SerializableItemStack, false)
+        }
+        val storageContents = Array(player.inventory.storageContents.size) {
+            inner.get("$prefix.${player.uniqueId}.storage.$it", ConfigDataType.SerializableItemStack, false)
+                ?: itemStack(Material.AIR)
+        }
+        val extraContents = Array(player.inventory.extraContents.size) {
+            inner.get("$prefix.${player.uniqueId}.extra.$it", ConfigDataType.SerializableItemStack, false)
+        }
+
+        player.inventory.apply {
+            setArmorContents(armorContents)
+            setStorageContents(storageContents)
+            setExtraContents(extraContents)
+        }
+        player.updateInventory()
     }
 
     fun reload() = inner.reload()
