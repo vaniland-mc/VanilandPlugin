dc56b741932639f8573e3ddd9c031520dcdd507d
rona-tombo <ronatombo@gmail.com>
2022-07-21 01:00:13 +0900

    ✨ Support MiniMessage on auto messages

---
+++ src/main/kotlin/land/vani/plugin/core/config/AutoMessagesConfig.kt
@@ -0,0 +1,35 @@
+package land.vani.plugin.core.config
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import land.vani.plugin.core.VanilandPlugin
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.configuration.file.YamlConfiguration
+import kotlin.io.path.div
+import kotlin.io.path.reader
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+
+class AutoMessagesConfig(plugin: VanilandPlugin) {
+    private val configPath = plugin.dataFolder.toPath() / "autoMessages.yml"
+    private val config = YamlConfiguration()
+
+    private val miniMessage = MiniMessage.miniMessage()
+
+    suspend fun reload() {
+        withContext(Dispatchers.IO) {
+            config.load(configPath.reader())
+        }
+    }
+
+    val period: Duration
+        get() = config.getInt("period").minutes
+
+    val prefix: Component
+        get() = config.getString("prefix")?.let { miniMessage.deserialize(it) }
+            ?: Component.empty()
+
+    val messages: List<Component>
+        get() = config.getStringList("messages").map { miniMessage.deserialize(it) }
+}
